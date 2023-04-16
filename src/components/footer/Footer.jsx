1280437a1771781ba6087441905af1c60236b8eb
Alexsander Gomes baptista <gomes.alexsander.hamir@outlook.com>
2023-04-15 17:41:57 -0700

    done

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import './Footer.css'
+import React from "react";
+import "./Footer.css";
 
 const Footer = () => {
   return (
-    <div className='footer__main'>Footer</div>
-  )
-}
+    <div className="footer__main">
+      <h1>A2G</h1>
+      <p>Committed to delivering excellence with precision.</p>
+      <p>Copyright © A2G All rights reserved.</p>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
