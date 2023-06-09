4319f13dc727242bd2d5e47c58cc471cf1361061
Alexsander Gomes <alexsandergomes@Alexsanders-MacBook-Pro.local>
2023-06-09 14:52:11 -0700

    done

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,7 +5,6 @@ const Footer = () => {
   return (
     <div className="footer__main">
       <h1>A2G</h1>
-      <p>Committed to delivering excellence with precision.</p>
       <p>Copyright © A2G All rights reserved.</p>
     </div>
   );
